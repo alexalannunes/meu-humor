fce256fb68aecf51cb00c4f9e2e5d791cd1330e2
Alex Alan Nunes <alexalannunes@gmail.com>
2025-09-26 16:10:24 -0300

    feat: integrate react-query for data fetching and update provider component

--- src/provider.tsx
+++ src/provider.tsx
@@ -2,19 +2,27 @@ import type { NavigateOptions } from "react-router-dom";
 
 import { HeroUIProvider } from "@heroui/system";
 import { useHref, useNavigate } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 declare module "@react-types/shared" {
   interface RouterConfig {
     routerOptions: NavigateOptions;
   }
 }
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export function Provider({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
 
   return (
     <HeroUIProvider navigate={navigate} useHref={useHref}>
-      {children}
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
     </HeroUIProvider>
   );
 }
