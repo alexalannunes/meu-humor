1529f824cf0322a669c64593c00e6bd779d4d07e
Alex Alan Nunes <alexalannunes@gmail.com>
2025-09-26 16:17:28 -0300

    refactor: clean up request and response interceptors in API setup

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,6 +10,12 @@ const api = axios.create({
 
 // TODO: improve interceptors for requests and responses
 
+api.interceptors.request.use((config) => {
+  // const token = getToken();
+  // if (token) config.headers['Authorization'] = `Bearer ${token}`;
+  return config;
+});
+
 api.interceptors.response.use(
   (response) => {
     return response;
@@ -20,6 +26,19 @@ api.interceptors.response.use(
       // clear stores, etc.
       // window.location.href = '/login'; // Example action
     }
+
+    // const originalRequest = error.config;
+
+    // if (error.response?.status === 401 && !originalRequest._retry) {
+    //   originalRequest._retry = true;
+    //   const newToken = await refreshToken();
+
+    //   if (newToken) {
+    //     originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+
+    //     return api(originalRequest);
+    //   }
+    // }
 
     return Promise.reject(error);
   },
